321391c09254cd0223aae235cabd25b5d1737b33
PaulineFss <pauline.fassy@outlook.fr>
2025-11-28 14:56:50 +0100

    Nouveau projet Super heros

---
+++ Super-heros/Super-heros/src/main/java/heros/SuperHerosApplication.java
@@ -0,0 +1,13 @@
+package heros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHerosApplication.class, args);
+	}
+
+}
