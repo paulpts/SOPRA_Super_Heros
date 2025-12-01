9456bd29b791ed397736cc396475bf4e7e987dac
Marwah47 <MarwahSofia777@gmail.com>
2025-12-01 10:21:08 +0100

    WIP on Marwah
    angular dto, service config , routes

--- heros-angular/src/app/app.routes.ts
+++ heros-angular/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './page/home-page/home-page';
+import { HerosPage} from './page/heros-page/heros-page';
+import { MissionPage } from './page/mission-page/mission-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirection par défaut vers /home si l'utilisateur écrit / dans l'URL
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // Tableau de bord
+  { path: 'home', component: HomePage },
+
+  // Gestion des héros 
+  { path: 'heros', component: HerosPage },
+
+  // Gestion des missions 
+  { path: 'mission', component: MissionPage },
+
+  // Redirection vers le dashboard si l'URL n'existe pas
+  { path: '**', redirectTo: 'home' }
+];
